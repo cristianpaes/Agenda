7e70ae8888499681e5586d93bb89ee55a4094115
cristianpaes <s4camargo@gmail.com>
2020-04-11 23:57:14 -0300

    progeto agenda DIO

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/cristian/Documentos/Python/Web/Django/teste/agenda01/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
